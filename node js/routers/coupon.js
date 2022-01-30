498060b2f9dcd6dedff2069b696e40fdb330af01
Chani Rozenberg <chanir@3base.co.il>
2022-01-30 11:35:01 +0200

    pagingBotton

--- node js/routers/coupon.js
+++ node js/routers/coupon.js
@@ -4,9 +4,9 @@ const couponModel = require('../models/coupon');
 const getData = require('./../getData/getData')
 
 router.get('/:strart' , async (request, response) => {
-    const coupons = await couponModel.paginate({},{offset: 0, limit: parseInt(request.params.strart) + 20})
-    //const coupons = await couponModel.find().skip(parseInt(request.params.strart)).limit(20)
+    //const coupons = await couponModel.paginate({},{offset: 0, limit: parseInt(request.params.strart) + 20})
     try{
+        const coupons = await couponModel.find().skip(parseInt(request.params.strart)).limit(20)
         response.send(coupons);
     }
     catch (error) {
--- react/src/components/coupon-list.jsx
+++ react/src/components/coupon-list.jsx
@@ -10,50 +10,54 @@ export default function CouponList() {
     const [edit, setEdit] = useState(false)
     const [couponToEdit, setCouponToEdit] = useState(null)
     const [start, setStart] = useState(0)
-    //const isDeleted = false
 
     const setEditToFalse = (coupon) => {
         editCoupon(coupon)
         setCoupons(coupons.map((c) => c._id === coupon._id ? coupon : c))
         setEdit(false)
     }
-    
+
     const getCouponList = () => {
+        debugger;
         setClicked(true)
         return fetch(`http://localhost:5050/coupon/${start}`)
-        .then(coupons => coupons.json())
-        .then(coupons => {
-            setCoupons(coupons.docs)
-        })
+            .then(c => c.json())
+            .then(c => {
+                setCoupons(c)
+            })
+            .then(_ => {
+                setStart(start + 10)
+                console.log(start)
+            })
     }
 
     const editCoupon = (coupon) => {
         return fetch(`http://localhost:5050/coupon/${coupon._id}`, {
             method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(coupon),
         })
-        .then(response => response.json())
+            .then(response => response.json())
     }
 
     const deleteCoupon = (id) => {
         return fetch(`http://localhost:5050/coupon/${id}`, {
             method: 'DELETE',
         })
-        .then(response => response.json())
-        .then(getCouponList())
+            .then(response => response.json())
+            .then(getCouponList())
     }
 
     const scrollBottom = () => {
         console.log("ffff");
-        setStart(start+10)
+        setStart(start + 10)
         console.log(start);
         getCouponList()
     }
 
     return (
         <div>
-            {edit && <Edit coupon={couponToEdit} setEditToFalse={setEditToFalse} /> }
+            {edit && <Edit coupon={couponToEdit} setEditToFalse={setEditToFalse} />}
             {!clicked &&
                 <button type="button" className="btn btn-light" onClick={() => {
                     getCouponList()
@@ -68,53 +72,55 @@ export default function CouponList() {
                 {coupons &&
                     <div>
                         <InfiniteScroll
-                            dataLength={coupons.length}
-                            next={scrollBottom}
+                            dataLength={start}
+                            //next={scrollBottom}
+                            next={getCouponList}
                             hasMore={true}
+                        //inverse={true}
                         >
-                        {<table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">check</th>
-                                    <th scope="col">coupon name</th>
-                                    <th scope="col">type</th>
-                                    <th scope="col">start date</th>
-                                    <th scope="col">end date</th>
-                                    <th scope="col">discount amount</th>
-                                    <th scope="col">user group name</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {coupons.map((coupon, index) =>
-                                (
-                                    <tr key={coupon._id}>
-                                        <th scope="row">{index + 1}</th>
-                                        <td><input
-                                            type="checkbox"
-                                            className="form-check-input"
-                                            id="exampleCheck1"
-                                            onChange={() => coupon.checked = !coupon.checked} /></td>
-                                        <td>{coupon.couponName}</td>
-                                        <td>{coupon.type}</td>
-                                        <td>{new Date(coupon.startDate).toLocaleDateString()}</td>
-                                        <td>{new Date(coupon.endDate).toLocaleDateString()}</td>
-                                        <td>{coupon.discountAmount}</td>
-                                        <td>{coupon.userGroupName}</td>
-                                        <td><button type="button" className="btn btn-light" onClick={() => {
-                                            setCouponToEdit(coupon)
-                                            setEdit(true)
-                                        }
-                                        }>edit</button></td>
-                                        <td><button type="button" className="btn btn-light" onClick={() => {
-                                            deleteCoupon(coupon._id)
-                                        }
-                                        }>delete</button></td>
+                            {<table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">check</th>
+                                        <th scope="col">coupon name</th>
+                                        <th scope="col">type</th>
+                                        <th scope="col">start date</th>
+                                        <th scope="col">end date</th>
+                                        <th scope="col">discount amount</th>
+                                        <th scope="col">user group name</th>
                                     </tr>
-                                )
-                                )}
-                            </tbody>
-                        </table>}
+                                </thead>
+                                <tbody>
+                                    {coupons.map((coupon, index) =>
+                                    (
+                                        <tr key={coupon._id}>
+                                            <th scope="row">{index + 1}</th>
+                                            <td><input
+                                                type="checkbox"
+                                                className="form-check-input"
+                                                id="exampleCheck1"
+                                                onChange={() => coupon.checked = !coupon.checked} /></td>
+                                            <td>{coupon.couponName}</td>
+                                            <td>{coupon.type}</td>
+                                            <td>{new Date(coupon.startDate).toLocaleDateString()}</td>
+                                            <td>{new Date(coupon.endDate).toLocaleDateString()}</td>
+                                            <td>{coupon.discountAmount}</td>
+                                            <td>{coupon.userGroupName}</td>
+                                            <td><button type="button" className="btn btn-light" onClick={() => {
+                                                setCouponToEdit(coupon)
+                                                setEdit(true)
+                                            }
+                                            }>edit</button></td>
+                                            <td><button type="button" className="btn btn-light" onClick={() => {
+                                                deleteCoupon(coupon._id)
+                                            }
+                                            }>delete</button></td>
+                                        </tr>
+                                    )
+                                    )}
+                                </tbody>
+                            </table>}
                         </InfiniteScroll>
                         <footer className="text-center">
                             <button type="btn btn-light" onClick={() => {
@@ -122,7 +128,7 @@ export default function CouponList() {
                             }}>delete</button>
                         </footer>
                     </div>
-                    }
+                }
             </div>
         </div>
     )
